0ccf6fa4b66558b1dbd0ceceeb8ab3a56f9fd372
jb <jessb.epicodus@gmail.com>
2022-02-04 12:28:34 -0800

    good fail test 2: LifeExpectancy returns years remaing where years lived is over 72

--- __tests__/life-expectancy-calculator.test.js
+++ __tests__/life-expectancy-calculator.test.js
@@ -5,13 +5,14 @@ describe('LifeExpectancy', () => {
   let testAge;
 
   beforeEach(() => {
-    testAge = new LifeExpectancy(73);
+    testAge = new LifeExpectancy(32);
   });
 
   test('should correctly return remaining years', () => {
     expect(testAge.getLifeExpectancy()).toEqual(41);
   });
   test('should correctly return remaining years', () => {
-    expect(testAge.getLifeExpectancy()).toEqual(0);
+    let oldAge = new LifeExpectancy(73)
+    expect(oldAge.getLifeExpectancy()).toEqual(0);
   });
 });
