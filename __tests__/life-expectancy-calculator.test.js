fe41c3b73656fcc58920a81495849f43eab266b6
jb <jessb.epicodus@gmail.com>
2022-02-04 16:23:30 -0800

    WIP refactoring LifeExptectancy test1

--- __tests__/life-expectancy-calculator.test.js
+++ __tests__/life-expectancy-calculator.test.js
@@ -8,39 +8,7 @@ describe('LifeExpectancy', () => {
     testAge = new LifeExpectancy(32);
   });
 
-  test('should correctly return remaining Mercury years', () => {
-    expect(testAge.getMercuryRemainigYrs()).toEqual("169 year(s) remaining");
-  });
-
-  test('should correctly return remaining Mercury years', () => {
-    let oldAge = new LifeExpectancy(73)
-    expect(oldAge.getMercuryRemainigYrs()).toEqual("2 year(s) past expected");
-  });
-
-  test('should correctly return remaining Venus years', () => {
-    expect(testAge.getVenusRemainigYrs()).toEqual("65 year(s) remaining");
-  });
-  
-  test('should correctly return remaining Venus years', () => {
-    let oldAge = new LifeExpectancy(73)
-    expect(oldAge.getVenusRemainigYrs()).toEqual("1 year(s) past expected");
-  });
-
-  test('should correctly return remaining Mars years', () => {
-    expect(testAge.getMarsRemainigYrs()).toEqual("22 year(s) remaining");
-  });
-
-  test('should correctly return remaining Mars years', () => {
-    let oldAge = new LifeExpectancy(73)
-    expect(oldAge.getMarsRemainigYrs()).toEqual("0 year(s) past expected");
-  });
-
-  test('should correctly return remaining Jupiter years', () => {
-    expect(testAge.getJupiterRemainigYrs()).toEqual("3 year(s) remaining");
-  });
-
-  test('should correctly return remaining Jupiter years', () => {
-    let oldAge = new LifeExpectancy(73)
-    expect(oldAge.getJupiterRemainigYrs()).toEqual("0 year(s) past expected");
+    test('should correctly return remaining Mercury years', () => {
+    expect(testAge.getLifeExpectency()).toEqual("169 year(s) remaining");
   });
 });
--- src/js/life-expectancy-calculator.js
+++ src/js/life-expectancy-calculator.js
@@ -1,42 +1,19 @@
 export default class LifeExpectancy {
 
   constructor(earthYrs) {
-    this.earthYrs = earthYrs;
+    this.earthYrs = 72;
+    this.mercuryYrs = earthYrs / .24
+    this.venusYrs = earthYrs / .62
+    this.marsYrs = earthYrs / 1.88
+    this.jupiterYrs = earthYrs / 11.86
   }
 
-  getMercuryRemainigYrs () {
-    let livedYrs = this.earthYrs;
-    if (livedYrs <= 72) {
-      return (Math.round((72.6 - livedYrs) / .24) + " year(s) remaining");
+  getLifeExpectency () {
+    let livedYrs = this.mercuryYrs;
+    if (livedYrs > 0) {
+      return (Math.round(livedYrs) + " year(s) remaining");
     } else {
-      return (Math.round((livedYrs - 72.6) / .24) + " year(s) past expected");
-    }
-  }
-
-  getVenusRemainigYrs () {
-    let livedYrs = this.earthYrs;
-    if (livedYrs <= 72) {
-      return (Math.round((72.6 - livedYrs) / .62) + " year(s) remaining");
-    } else {
-      return (Math.round((livedYrs - 72.6) / .62) + " year(s) past expected");
-    }
-  }
-
-  getMarsRemainigYrs () {
-    let livedYrs = this.earthYrs;
-    if (livedYrs <= 72) {
-      return (Math.round((72.6 - livedYrs) / 1.88) + " year(s) remaining");
-    } else {
-      return (Math.round((livedYrs - 72.6) / 1.88) + " year(s) past expected");
-    }
-  }
-
-  getJupiterRemainigYrs () {
-    let livedYrs = this.earthYrs;
-    if (livedYrs <= 72) {
-      return (Math.round((72.6 - livedYrs) / 11.86) + " year(s) remaining");
-    } else {
-      return (Math.round((livedYrs - 72.6) / 11.86) + " year(s) past expected");
+      return (Math.round(livedYrs) + " year(s) past expected");
     }
   }
 }
